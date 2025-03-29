91da669f56c1b773d618bf56647b6998ab689487
unknown <you@example.com>
2025-03-29 17:14:02 +0300

    dot - routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import FreeMap from "./components/FreeMap";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import FreeMap from "./components/FreeMap"
 
 function App() {
   return (
- <div>
-  <FreeMap/>
- </div>
-  
-  );
+    <Router>
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgotPassword" component={ForgotPassword} />
+        <Route path="/resetPassword" component={ResetPassword} />
+        <Route path="/" component={FreeMap} exact />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
